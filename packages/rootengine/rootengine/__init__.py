36d3511bf1a1f2c3d138c5a426f907513d3ddc4e
zimvir <zimvir@qq.com>
2026-04-14 23:00:11 +0800

    版本管理从这次提交重新开始，整理版本混乱， 弃用generate_changelog.py
    用于generate_changelog.py，导致git log 出现混乱（最主要的问题就是误删了两次提交），于是我恢复到误删了那两次提交前的状态，重新开始版本管理，短期先用打tag管理monorepo的版本

--- packages/rootengine/rootengine/__init__.py
+++ packages/rootengine/rootengine/__init__.py
@@ -1,2 +1,1 @@
 from rootengine_core.utils import generate_json_dict
-#generate_json_dict(input_dir=r"D:\GitHub_Project\RootEngine\packages\rootengine\rootengine\schema\source",output_py=r"D:\GitHub_Project\RootEngine\packages\rootengine\rootengine\schema\runtime\frame_schema.py")
